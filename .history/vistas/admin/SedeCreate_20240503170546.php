c41b8b93ba38183fe9ed70124b07b2933713f1be
Jesus.Santana <jfsantana77@gmail.com>
2024-05-03 18:12:08 -0400

    menu LIST listo para facilitadores aprendices representante roles sedes
    CREACION DE SEDE Y EDICION LISTO

---
+++ .history/vistas/admin/SedeCreate_20240503170546.php
@@ -0,0 +1,146 @@
+<?php
+if (!isset($_SESSION)) {
+  session_start();
+}
+if (!isset($_SESSION['id_user'])) {
+  header("Location:  http://" . $_SERVER['HTTP_HOST']);
+  exit();
+}
+require_once '../funciones/wsdl/clases/consumoApi.class.php';
+
+ //print("<pre>".print_r(($_POST) ,true)."</pre>"); //die;
+$token='';
+$URL        = "http://" . $_SERVER['HTTP_HOST'] . "/funciones/wsdl/empleados?aprobadores";
+$rs         = API::GET($URL, $token);
+$arrayAprobadores  = API::JSON_TO_ARRAY($rs);
+
+ //print("<pre>".print_r(($arrayAprobadores) ,true)."</pre>"); //die;
+
+if (@$_POST['mod'] == 1) {
+  $accion = "Crear";
+} elseif(@$_POST['mod'] == 2) {
+  $accion = "Editar";
+  //Listado Sede
+  $id = @$_POST["id"];
+  $token = $_SESSION['token'];
+  $URL        = "http://" . $_SERVER['HTTP_HOST'] . "/funciones/wsdl/sede?type=1&idSede=$id";
+  $rs         = API::GET($URL, $token);
+  $arrayCconsultora  = API::JSON_TO_ARRAY($rs);
+
+  $nombreSede = $arrayCconsultora[0]['nombreSede'];
+  //rifSede
+  //paisSede
+  //ciudadSede
+  //direccionSede
+  //telefonoSede
+  //emailSede
+  //estado
+  if ($arrayCconsultora[0]['estado'] == 'Activo')
+    $estado = 1;
+  else
+    $estado = 0;
+}else{
+  $accion = "Crear";
+  @$_POST['mod'] = 1;
+}
+?>
+
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0">Sede</h1>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Main content -->
+<form action="../funciones/funcionesGenerales/XM_sede.model.php" method="post" name="consultora" id="consultora">
+  <input type="hidden" name="mod" value="<?php echo @$_POST['mod'] ?>">
+  <input type="hidden" name="idSede" value="<?php echo @$_POST['id'] ?>">
+  <div class="container-fluid">
+
+    <div class="row">
+      <div class="col-lg-12 col-12">
+
+        <div class="card card-primary">
+          <div class="card-header">
+            <h3 class="card-title"><?php echo $accion; ?> Sede</h3>
+          </div>
+          <div class="card-body">
+              <div class="row">
+                <div class="col-sm-4">
+                  <label for="rifSede">Rif - Codigo Fiscal</label>
+                  <input type="text" class="form-control" require name="rifSede" id="rifSede" placeholder="Rif" value="<?php echo @$rifSede; ?>">
+                </div>
+                <div class="col-sm-4">
+                  <label for="nombreSede">Nombre Sede</label>
+                  <input type="text" class="form-control" require name="nombreSede" id="nombreSede" placeholder="Nombre de la Sede" value="<?php echo @$nombreSede; ?>" >
+                </div>
+                <div class="col-sm-4">
+                  <label for="telefonoSede">Telefono Sede</label>
+                  <input type="text" class="form-control" require name="telefonoSede" id="telefonoSede" placeholder="Nombre de la Sede" value="<?php echo @$telefonoSede; ?>">
+                </div>
+                <div class="col-sm-4">
+                  <label for="emailSede">Em@il</label>
+                  <input type="email" class="form-control" require name="emailSede" id="emailSede" placeholder="Nombre de la Sede" value="<?php echo @$emailSede; ?>">
+                </div>
+                <div class="col-sm-4">
+                  <label for="nombreCliente">Pais</label>
+                  <select class="form-control" name="paisSede" id="paisSede" require>
+                    <option >Seleccione</option>
+                    <option <?php if (@$estado == 'Chile') {
+                              echo 'selected';
+                            } ?> value="Chile">Chile</option>
+                    <option <?php if (@$paisSede == 'Venezuela') {
+                              echo 'selected';
+                            } ?> value="Venezuela">Venezuela</option>
+                  </select>
+                </div>
+                <div class="col-sm-4">
+                  <label for="nombreCliente">Ciudad</label>
+                  <select class="form-control" name="ciudadSede" id="ciudadSede" require>
+                    <option >Seleccione</option>
+                    <option <?php if (@$estado == 'Carabobo') {
+                              echo 'selected';
+                            } ?> value="Carabobo">Carabobo</option>
+                    <option <?php if (@$paisSede == 'Santiago') {
+                              echo 'selected';
+                            } ?> value="Santiago">Santiago</option>
+                  </select>
+                </div>
+                <div class="col-sm-12">
+                    <label for="nombreCliente">Direccion</label>
+                    <textarea id="summernote" name="direccionSede">
+
+                    </textarea>
+                </div>
+                <div class="col-sm-4">
+                  <label>Estado</label>
+                  <select class="form-control" name="activo" id="activo" required>
+                    <option >Seleccione</option>
+                    <option <?php if (@$estado == 1) {
+                              echo 'selected';
+                            } ?> value=1>Activo</option>
+                    <option <?php if (@$estado == 0) {
+                              echo 'selected';
+                            } ?> value=0>Desactivado</option>
+                  </select>
+                </div>
+              </div>
+          </div>
+        </div>
+
+
+        <div class="card-footer">
+          <button type="submit" class="btn btn-primary"><?php echo $accion; ?></button>
+        </div>
+    </div>
+  </div>
+<!-- ./col -->
+
+<!-- /.row (main row) -->
+  </div><!-- /.container-fluid -->
+  </section>
+</form>
---
+++ .history/vistas/admin/usuarioList_20240503103230.php
@@ -0,0 +1,149 @@
+<?php
+if (!isset($_SESSION)) {
+  session_start();
+}
+if (!isset($_SESSION['id_user'])) {
+  header("Location:  http://" . $_SERVER['HTTP_HOST']);
+  exit();
+}
+require_once '../funciones/wsdl/clases/consumoApi.class.php';
+
+//Listado Clientes
+$id_usu = @$_POST["id_usu"];
+$token = $_SESSION['token'];
+$URL        = "http://" . $_SERVER['HTTP_HOST'] . "/funciones/wsdl/empleados?type=1";
+$rs         = API::GET($URL, $token);
+$arrayUsuarios  = API::JSON_TO_ARRAY($rs);
+
+
+//print("<pre>".print_r(($URL) ,true)."</pre>"); //die;
+
+?>
+<!-- Content Header (Page header) -->
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0">Usuarios</h1>
+        <h5 class="m-0">(Facilitadores/Coordinador/Administrador)</h5>
+      </div><!-- /.col -->
+    </div><!-- /.row -->
+  </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<!-- Main content -->
+<section class="content">
+  <div class="container-fluid">
+    <!-- Small boxes (Stat box) -->
+    <div class="row">
+
+    <?php
+    // indicador de usuarios por rol
+      $URL        = "http://" . $_SERVER['HTTP_HOST'] . "/funciones/wsdl/rol?type=1";
+      $rs         = API::GET($URL, $token);
+      $arrayRoles  = API::JSON_TO_ARRAY($rs);
+
+      foreach ($arrayRoles as $rol) {
+
+        $URL        = "http://" . $_SERVER['HTTP_HOST'] . "/funciones/wsdl/rol?type=2&idRol=".$rol['idRol'];
+        $rs         = API::GET($URL, $token);
+        $arrayUsuarioRol  = API::JSON_TO_ARRAY($rs);
+        ?>
+
+
+
+
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-warning">
+          <div class="inner">
+            <h3><?php echo count($arrayUsuarioRol); ?></h3>
+
+            <p><?php echo 'Rol: '.$rol['descripcionRol']; ?></p>
+          </div>
+          <div class="icon">
+          <i class="ion " ><ion-icon name="people-outline"></ion-icon></ion-icon></i>
+          </div>
+          <a href="#" onclick="enviarParametrosGetsionCreate('admin/usuarioCreate.php','1')" class="small-box-footer">Crear Consultores <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <?php } ?>
+
+      <!-- ./col -->
+    </div>
+    <!-- /.row -->
+
+
+
+    <!-- Main row -->
+    <div class="row">
+      <!-- Left col -->
+      <section class="col-lg-12 connectedSortable">
+        <!-- Custom tabs (Charts with tabs)-->
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Listado de Usuarios</h3>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body">
+            <table id="example1" class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Rol</th>
+                  <th>Nombre Y apellido</th>
+                  <th>User</th>
+                  <th>Activo</th>
+                  <th>Telefono</th>
+                  <th>Cedula</th>
+                  <th>Email</th>
+                  <th>Cargo</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php foreach ($arrayUsuarios as $consultores) {?>
+                  <tr>
+                    <td><a href="#" onclick="enviarParametrosGetsionUpdate('admin/usuarioCreate.php',2,'<?php echo $consultores['idUsuario']; ?>')" class="nav-link "><?php echo $consultores['idUsuario'];?></a></td>
+                    <td><?php echo $consultores['descripcionRol'];?></td>
+                    <td><?php echo $consultores['apellidoUsuario'].', '.$consultores['nombreUsuario'];?></td>
+                    <td><?php echo $consultores['loginUsuario'];?></td>
+                    <td><?php echo $consultores['activoUsuario'];?></td>
+                    <td><?php echo $consultores['telefonoUsuario'];?></td>
+                    <td><?php echo $consultores['cedulaUsuario'];?></td>
+                    <td><?php echo $consultores['emailUsuario'];?></td>
+                    <td><?php echo $consultores['cargoUsuario'];?></td>
+                  </tr>
+                <?php } ?>
+
+              </tbody>
+              <tfoot>
+                <tr>
+                <th>Id</th>
+                  <th>Rol</th>
+                  <th>Nombre Y apellido</th>
+                  <th>User</th>
+                  <th>Activo</th>
+                  <th>Telefono</th>
+                  <th>Cedula</th>
+                  <th>Email</th>
+                  <th>Cargo</th>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+
+
+        <!-- /.card -->
+      </section>
+      <!-- /.Left col -->
+      <!-- right col (We are only adding the ID to make the widgets sortable)-->
+
+      <!-- right col -->
+    </div>
+    <!-- /.row (main row) -->
+  </div><!-- /.container-fluid -->
+</section>
