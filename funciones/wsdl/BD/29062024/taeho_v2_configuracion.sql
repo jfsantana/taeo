d057f40f9c9d9722ae50cb89bad5aa0874fdbb46
victori4santana <vicky200715@gmail.com>
2024-06-29 14:29:20 -0400

    base de datos 2906 consulta de las taeho_v2_planificacion_items tabal de configuracion para el envio de correo tabla de cabecera planificacion

---
+++ funciones/wsdl/BD/29062024/taeho_v2_configuracion.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: taeho_v2
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `configuracion`
+--
+
+DROP TABLE IF EXISTS `configuracion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `configuracion` (
+  `idConfiguracion` int NOT NULL AUTO_INCREMENT,
+  `campo` varchar(45) DEFAULT NULL,
+  `valor` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idConfiguracion`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `configuracion`
+--
+
+LOCK TABLES `configuracion` WRITE;
+/*!40000 ALTER TABLE `configuracion` DISABLE KEYS */;
+INSERT INTO `configuracion` VALUES (1,'SMTP_server','mail.organizaciontaeo.com'),(2,'SMTP_usuario','info@organizaciontaeo.com'),(3,'SMTP_clave','_9H)GfPp(~w_'),(4,'SMTP_puerto','587'),(5,'SMTP_TLS','si');
+/*!40000 ALTER TABLE `configuracion` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-29 14:28:21
---
+++ funciones/wsdl/BD/29062024/taeho_v2_objetivo_item_v2.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: taeho_v2
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `objetivo_item_v2`
+--
+
+DROP TABLE IF EXISTS `objetivo_item_v2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `objetivo_item_v2` (
+  `idItems` int NOT NULL AUTO_INCREMENT,
+  `idHeader` int DEFAULT NULL,
+  `idItemsPadre` int DEFAULT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  `jerarquia` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idItems`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `objetivo_item_v2`
+--
+
+LOCK TABLES `objetivo_item_v2` WRITE;
+/*!40000 ALTER TABLE `objetivo_item_v2` DISABLE KEYS */;
+INSERT INTO `objetivo_item_v2` VALUES (1,1,NULL,'NodoPadre',''),(2,1,1,'OPERANTES VERBALES VOCALES (se debe registrar','0'),(3,1,2,'EMPAREJAMIENTO ESTÍMULO-ESTÍMULO (APOYAR GENE','0.1'),(4,1,2,'ECOICAS (APOYAR GENERALIZACIÓN A OTROS CONTEX','0.2'),(5,1,2,'TACTOS (APOYAR GENERALIZACIÓN A OTROS CONTEXT','0.3'),(6,1,2,'AUTOCLÍTICOS (APOYAR GENERALIZACIÓN A OTROS C','0.4'),(7,1,2,'INTRAVERBALES (APOYAR GENERALIZACIÓN A OTROS ','0.5'),(8,1,2,'MANDOS (APOYAR GENERALIZACIÓN A OTROS CONTEXT','0.6'),(9,1,1,'SEGMENTOS FINOS','1'),(10,1,9,'AMASAR (DEBE INCLUIR EL PULGAR EN EL AMASADO)','1.1'),(11,1,10,'NIVEL INICIAL (ANTEBRAZO A NIVEL DE LA MESA)','1.1.1'),(12,1,11,'Amasar con mano derecha 5 veces','1.1.1.1'),(13,1,11,'Amasar con mano izquierda 5 veces','1.1.1.2'),(14,1,11,'Amasar con mano derecha 8 veces','1.1.1.3'),(15,1,11,'Amasar con mano izquierda 8 veces','1.1.1.4');
+/*!40000 ALTER TABLE `objetivo_item_v2` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-29 14:28:22
